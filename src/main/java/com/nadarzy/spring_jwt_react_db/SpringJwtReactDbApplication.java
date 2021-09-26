43087c3a1dfb53ced5d3f1541fea444085640718
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-26 10:43:38 +0200

    Initial commit

---
+++ src/main/java/com/nadarzy/spring_jwt_react_db/SpringJwtReactDbApplication.java
@@ -0,0 +1,13 @@
+package com.nadarzy.spring_jwt_react_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtReactDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtReactDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nadarzy/spring_jwt_react_db/SpringJwtReactDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadarzy.spring_jwt_react_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtReactDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
